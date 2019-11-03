1d328e8dba18f35701cf4e1f920d6190e8c3e796
Eric <ep00959@georgiasouthern.edu>
2019-11-03 15:31:30 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,26 @@
+
+
+// array to store card values
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+} 
+else	{
+	alert("select another card to start.");
+}
+
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
 
-console.log("User flipped " + cardOne);
